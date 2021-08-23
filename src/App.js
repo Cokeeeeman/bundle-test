41d6029daa9f2d87be2373c0de65dd801c2e6518
Max Chen <zchen@spokeo.com>
2021-08-23 14:53:20 -0700

    add access to sample data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { Button } from "@spokeo/styleguide/inputs";
+import { copyStyles } from "@spokeo/styleguide/typography";
 import { gray200, xl7Space } from "@spokeo/styleguide";
 
 const Wrapper = styled.div`
@@ -8,8 +8,15 @@ const Wrapper = styled.div`
   padding: ${xl7Space};
 `;
 
+const P = styled.p`
+  ${copyStyles({ version: 3 })}
+`;
 export default () => (
   <Wrapper>
-    <Button purchase>button</Button>
+    <h2>sample data is accesable globally via variable "data"</h2>
+    <P>
+      data.BundleComponents.BundleComponent.length:{" "}
+      {data.BundleComponents.BundleComponent.length}
+    </P>
   </Wrapper>
 );
